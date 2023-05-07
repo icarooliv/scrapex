8a2ec39686a46e290d9061b8fd45cd162095de1c
Icaro Oliveira <icarooliv2@gmail.com>
2023-05-07 13:17:54 -0300

    adds initial tests

--- test/scrapex_test.exs
+++ test/scrapex_test.exs
@@ -2,7 +2,11 @@ defmodule ScrapexTest do
   use ExUnit.Case
   doctest Scrapex
 
-  test "greets the world" do
-    assert Scrapex.hello() == :world
+  test "receives a valid url" do
+    assert {:ok, _} = Scrapex.scrape("https://www.google.com")
+  end
+
+  test "returns error for an invalid url" do
+    assert {:error, _} = Scrapex.scrape("https://www.google.com")
   end
 end
