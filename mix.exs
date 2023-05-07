8e88e22b20871c5d0dae4d9d87afb91de2652a5b
Icaro Oliveira <icarooliv2@gmail.com>
2023-05-07 15:49:23 -0300

    adds mox lib

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Scrapex.MixProject do
       {:floki, "~> 0.30.0"},
       {:httpoison, "~> 2.1"},
       {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:mox, "~> 1.0.2", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
