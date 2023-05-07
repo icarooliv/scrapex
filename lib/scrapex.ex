cd5599c96128ca795b44129b2733aa108c321851
Icaro Oliveira <icarooliv2@gmail.com>
2023-05-07 16:56:26 -0300

    scrapes data from html

--- lib/scrapex.ex
+++ lib/scrapex.ex
@@ -5,28 +5,23 @@ defmodule Scrapex do
 
   alias Scrapex.Http
 
-  def run(url) do
-    case normalize_url(url) do
-      {:ok, url} ->
-        scrape(url)
-
-      {:error, message} ->
-        {:error, message}
+  def run(url, tag_attrs) do
+    with {:ok, url} <- normalize_url(url),
+         {:ok, %{body: body}} <- Http.Client.get(url) do
+      scrape(body, tag_attrs, url)
     end
   end
 
-  defp scrape(url) do
-    case Http.Client.get(url) do
-      {:ok, %{status_code: 200, body: body}} ->
-        {:ok, body}
-
-      {:error, _} ->
-        {:error, :scrape_failed}
-    end
+  defp scrape(body, tag_attrs, url) do
+    tag_attr_results = extract_tag_attrs(body, tag_attrs, url)
+    {:ok, tag_attr_results}
   end
 
   defp normalize_url(url) do
     cond do
+      is_nil(url) ->
+        {:error, :invalid_format}
+
       valid_url?(url) ->
         {:ok, url}
 
@@ -38,11 +33,48 @@ defmodule Scrapex do
     end
   end
 
+  defp extract_tag_attrs(body, tag_attrs, url) do
+    Enum.reduce(tag_attrs, %{}, fn {key, tag, attr}, acc ->
+      extracted_values = extract_values(body, {tag, attr})
+
+      extracted_values =
+        cond do
+          attr in ["href", "src"] -> Enum.map(extracted_values, &prepend_url(&1, url))
+          true -> extracted_values
+        end
+
+      Map.put(acc, key, extracted_values)
+    end)
+  end
+
+  defp extract_values(body, {tag, attr}) do
+    body
+    |> Floki.parse_document!()
+    |> Floki.find(tag)
+    |> Floki.attribute(attr)
+    |> Enum.filter(&String.valid?/1)
+  end
+
+  defp prepend_url(maybe_path, url) do
+    case URI.parse(maybe_path) do
+      %URI{scheme: nil, path: path, fragment: fragment} ->
+        partial = path || fragment || url
+
+        URI.merge(url, partial)
+        |> URI.to_string()
+
+      _ ->
+        maybe_path
+    end
+  end
+
   defp without_scheme?(url) do
     is_nil(URI.parse(url).scheme)
   end
 
   defp valid_url?(url) when is_binary(url) do
-    URI.parse(url).scheme in ~w(http https)
+    uri = URI.parse(url)
+
+    uri.host && uri.host =~ "." && uri.scheme in ~w(http https)
   end
 end
--- test/scrapex_test.exs
+++ test/scrapex_test.exs
@@ -2,31 +2,97 @@ defmodule ScrapexTest do
   use ExUnit.Case, async: true
   import Mox
 
-  # setup do
-  #   %{
-  #     successful_response: File.read("/test/support/fixtures/200.html")
-  #   }
-  # end
   setup :verify_on_exit!
 
-  test "receives a valid url" do
-    response = File.read("/test/support/fixtures/200.html")
-    mock_get(response)
+  @html File.read!("test/support/fixtures/index.html")
 
-    assert {:ok, _} = Scrapex.run("https://www.example.com")
-    assert {:ok, _} = Scrapex.run("http://www.example.com")
-    assert {:ok, _} = Scrapex.run("example.com")
+  @tag_attrs [
+    {"links", "a", "href"},
+    {"assets", "img", "src"}
+  ]
+
+  test "receives a valid https url" do
+    mock_get(@html)
+
+    expected =
+      {:ok,
+       %{
+         "assets" => [
+           "https://www.example.com/image1.jpg",
+           "https://www.example.com/images/image2.jpg",
+           "https://www.example.com/images/image3.jpg"
+         ],
+         "links" => [
+           "https://www.example.com",
+           "https://www.example.com/about",
+           "https://www.example.com/contact",
+           "mailto:contact@example.com"
+         ]
+       }}
+
+    assert ^expected = Scrapex.run("https://www.example.com", @tag_attrs)
   end
 
-  test "returns error for not http/https urls" do
-    assert {:error, :invalid_format} = Scrapex.run("ws://javascript.info")
+  test "receives a valid http url" do
+    mock_get(@html)
+
+    expected =
+      {:ok,
+       %{
+         "assets" => [
+           "https://www.example.com/image1.jpg",
+           "http://www.example.com/images/image2.jpg",
+           "http://www.example.com/images/image3.jpg"
+         ],
+         "links" => [
+           "https://www.example.com",
+           "http://www.example.com/about",
+           "http://www.example.com/contact",
+           "mailto:contact@example.com"
+         ]
+       }}
+
+    assert ^expected = Scrapex.run("http://www.example.com", @tag_attrs)
+  end
+
+  test "accepts a url without scheme" do
+    mock_get(@html)
+
+    expected =
+      {:ok,
+       %{
+         "assets" => [
+           "https://www.example.com/image1.jpg",
+           "https://www.example.com/images/image2.jpg",
+           "https://www.example.com/images/image3.jpg"
+         ],
+         "links" => [
+           "https://www.example.com",
+           "https://www.example.com/about",
+           "https://www.example.com/contact",
+           "mailto:contact@example.com"
+         ]
+       }}
+
+    assert ^expected = Scrapex.run("www.example.com", @tag_attrs)
+  end
+
+  test "returns error for invalid urls" do
+    assert {:error, :invalid_format} = Scrapex.run("ws://javascript.info", @tag_attrs)
 
     assert {:error, :invalid_format} =
-             Scrapex.run("foo://example.com:8042/over/there?name=ferret#nose")
+             Scrapex.run("foo://example.com:8042/over/there?name=ferret#nose", @tag_attrs)
+
+    assert {:error, :invalid_format} = Scrapex.run("", @tag_attrs)
+
+    assert {:error, :invalid_format} = Scrapex.run("invalid_url", @tag_attrs)
+    assert {:error, :invalid_format} = Scrapex.run("https://invalid_url", @tag_attrs)
+
+    assert {:error, :invalid_format} = Scrapex.run(nil, @tag_attrs)
   end
 
   def mock_get(response) do
     Scrapex.Http.ClientMock
-    |> expect(:get, 3, fn _, _, _ -> {:ok, %{body: response, status_code: 200}} end)
+    |> expect(:get, fn _, _, _ -> {:ok, %{body: response, status_code: 200}} end)
   end
 end
