e60b98afa9d193aca3d1aa3dec474ec726debb3b
Icaro Oliveira <icarooliv2@gmail.com>
2023-05-07 13:29:23 -0300

    adds more assertions to tests

--- test/scrapex_test.exs
+++ test/scrapex_test.exs
@@ -4,9 +4,15 @@ defmodule ScrapexTest do
 
   test "receives a valid url" do
     assert {:ok, _} = Scrapex.run("https://www.example.com")
+    assert {:ok, _} = Scrapex.run("http://www.example.com")
   end
 
-  test "returns error for an invalid url" do
+  test "returns error for not http/https urls" do
+    assert {:error, _} = Scrapex.run("ws://javascript.info")
+    assert {:error, _} = Scrapex.run("foo://example.com:8042/over/there?name=ferret#nose")
+  end
+
+  test "returns error for malformed urls" do
     assert {:error, _} = Scrapex.run("www.example.com")
   end
 end
